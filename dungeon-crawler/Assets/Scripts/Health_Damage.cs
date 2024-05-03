b4a17ce7aa9a35de4f78e647c4cb43dfb72a1221
JiroudiR <112195052+JiroudiR@users.noreply.github.com>
2024-05-03 14:41:42 -0700

    Desktop 5/3 (TMPro and Healthbar Counter)

--- dungeon-crawler/Assets/Scripts/Health_Damage.cs
+++ dungeon-crawler/Assets/Scripts/Health_Damage.cs
@@ -7,35 +7,26 @@ public class Health_Damage : MonoBehaviour
 {
     public int health = 3;
     private UIManager uiManager;
-    private bool isDead = false;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        uiManager = FindObjectOfType<UIManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        uiManager = GetComponent<UIManager>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Damage();
+            InflictDamage();
         }
         Debug.Log(health);
     }
 
-    private void Damage()
+    private void InflictDamage()
     {
         health--;
         uiManager.SetHealth(health);
         if (health == 0)
         {
-            isDead = true;
             Destroy(this.gameObject);
         }
     }
--- dungeon-crawler/Assets/Scripts/UIManager.cs
+++ dungeon-crawler/Assets/Scripts/UIManager.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public Slider healthBar;
+    public GameObject fillArea;
+    public TMP_Text healthCount;
 
     public void SetHealth(int health)
     {
         healthBar.value = health;
+        if (health == 0)
+        {
+            Destroy(fillArea);
+        }
+    }
+
+    public void SetHealthCount()
+    {
+        
     }
 }
