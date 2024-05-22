d0d1d19fb17c7f33c681aad1c2aca3b6566b37f7
OkropiridzeReziko <142932058+OkropiridzeReziko@users.noreply.github.com>
2024-05-22 16:55:03 -0700

    GameWinText and Script
    5/22/2024

---
+++ dungeon-crawler/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WinScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text WINTEXT;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Win")
+        {
+            WINTEXT.gameObject.SetActive(true);
+        }
+      
+    }
+}
